2c536eb64943fff8bdd8da881c430f07bf0b4625
karetskiiVO <kar.vladi@yandex.ru>
2025-04-13 18:14:48 +0300

    Update slices.go

--- slices.go
+++ slices.go
@@ -11,25 +11,21 @@ func Map[S ~[]FromT, FromT, ToT any](s S, f func(FromT) ToT) []ToT {
 	return res
 }
 
-func Join[S1 ~[]T1, S2 ~[]T2, T1, T2 any](s1 S1, s2 S2) []struct {
-	fst T1
-	snd T2
-} {
+type Pair[T1, T2 any] struct {
+	First  T1
+	Second T2
+}
+
+func Join[S1 ~[]T1, S2 ~[]T2, T1, T2 any](s1 S1, s2 S2) []Pair[T1, T2] {
 	if len(s1) != len(s2) {
 		panic(fmt.Sprintf("in join: slices have different length s1[%v] s2[%v]", len(s1), len(s2)))
 	}
-	res := make([]struct {
-		fst T1
-		snd T2
-	}, len(s1))
+	res := make([]Pair[T1, T2], len(s1))
 
 	for i := range res {
-		res[i] = struct {
-			fst T1
-			snd T2
-		}{
-			fst: s1[i],
-			snd: s2[i],
+		res[i] = Pair[T1, T2]{
+			First:  s1[i],
+			Second: s2[i],
 		}
 	}
 
